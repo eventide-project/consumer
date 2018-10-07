c45ee98dd0c16dc768500f89a7448f30590f74a7
Nathan Ladd <nathanladd+github@gmail.com>
2018-10-07 10:38:13 -0500

    Incrementing example allows position update interval to be set

--- test/interactive/incrementing_example.rb
+++ test/interactive/incrementing_example.rb
@@ -1,7 +1,9 @@
 require_relative './interactive_init'
 
+position_update_interval = ENV['POSITION_UPDATE_INTERVAL']&.to_i
+
 Actor::Supervisor.start do
   stream_name = Controls::StreamName.example
 
-  Controls::Consumer::Incrementing.start(stream_name)
+  Controls::Consumer::Incrementing.start(stream_name, position_update_interval: position_update_interval)
 end
