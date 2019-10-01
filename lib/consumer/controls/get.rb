3b4db328bfaa1a43cdbe91f07f33b266cf4042bd
Nathan Ladd <nathanladd+github@gmail.com>
2019-10-01 16:45:47 -0500

    Get control is reworked to make use of Message Store get control

--- lib/consumer/controls/get.rb
+++ lib/consumer/controls/get.rb
@@ -1,53 +1,5 @@
 module Consumer
   module Controls
-    module Get
-      def self.example(batch_size: nil)
-        Example.build(batch_size: batch_size)
-      end
-
-      def self.default_batch_size
-        MessageData::Batch.size
-      end
-
-      class Example
-        include ::Configure
-
-        include MessageStore::Get
-
-        initializer :batch_size
-
-        configure :get
-
-        def self.build(batch_size: nil)
-          batch_size ||= Get.default_batch_size
-
-          new(batch_size)
-        end
-
-        def call(stream_name, position: nil)
-          position ||= 0
-
-          stream = streams.fetch(stream_name) do
-            return nil
-          end
-
-          stream[position]
-        end
-
-        def set_batch(stream_name, batch, position=nil)
-          position ||= 0
-
-          stream = streams[stream_name]
-
-          stream[position] = batch
-        end
-
-        def streams
-          @streams ||= Hash.new do |hash, key|
-            hash[key] = {}
-          end
-        end
-      end
-    end
+    Get = MessageStore::Controls::Get
   end
 end
