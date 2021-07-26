00ca0f462aaa605886ec75d1c91d27872db8c373
Nathan Ladd <nathanladd@gmail.com>
2021-07-26 09:52:42 -0500

    Supplemental settings tests are corrected

--- lib/consumer/controls/handle/settings.rb
+++ lib/consumer/controls/handle/settings.rb
@@ -1,7 +1,26 @@
 module Consumer
   module Controls
     module Handle
-      Settings = ::Messaging::Controls::Handler::Settings
+      module Settings
+        Error = Class.new(RuntimeError)
+
+        class Example
+          include Messaging::Handle
+
+          include ::Settings::Setting
+          setting :some_setting
+
+          attr_accessor :some_other_setting
+
+          def configure(settings:)
+            settings.set(self)
+          end
+
+          def handle(message_data)
+            raise Settings::Error if some_setting.nil?
+          end
+        end
+      end
     end
   end
 end
