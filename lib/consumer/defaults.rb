5dff4a2284fa416ac6f35a0f6744c3d109794c2c
Brad Robertson <brad@getirwin.com>
2022-06-20 11:54:32 -0400

    Position interval default can be set via ENV

--- lib/consumer/defaults.rb
+++ lib/consumer/defaults.rb
@@ -1,6 +1,9 @@
 module Consumer
   module Defaults
     def self.position_update_interval
+      env_interval = ENV['POSITION_UPDATE_INTERVAL']
+      return env_interval.to_i if !env_interval.nil?
+
       100
     end
   end
